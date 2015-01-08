c0080e8bcecf5bf1bd3b359069e70f03c4e1fdba
Nicolas VAREILLE <nicolas.vareille@epitech.eu>
2015-01-08 05:40:56 -0800

    Add images examples

---
+++ simple_image_demo/src/main.c
@@ -0,0 +1,64 @@
+#include "Khelljyr/Khelljyr.h"
+
+typedef struct	s_UserData
+{
+  Img		*img;
+}		UserData;
+
+static void	graphic_callback1(Layer *l, GContext *ctx)
+{
+  // Using USER_PTR allows us to get our datas given to app_init
+  UserData	*user = USER_PTR;
+
+  // We draw the image (initially, it is located on x: 0 y: 0)
+  draw_image(user->img, ctx);
+
+  // We draw the image at custom localisation
+  draw_image_pos(user->img, 80, 50, ctx);
+
+  // Some verbose for the watch
+  putstr("<= draw_image", 60, 20, ctx);
+  putstr("draw_image_pos =>", 0, 50, ctx);
+
+  // Credits
+  putstr("Made easily with\nKhelljyr Framework by\nNicolas VAREILLE", 0, 105, ctx);
+}
+
+static void	graphic_callback2(Layer *l, GContext *ctx)
+{
+  // Using USER_PTR allows us to get our datas given to app_init
+  UserData	*user = USER_PTR;
+
+  // We draw the image with effect(initially, it is located on x: 0 y: 0)
+  draw_image_effect(user->img, GCompOpAssignInverted, ctx);
+
+  // We draw the image  effect at custom localisation
+  draw_image_effect_pos(user->img, GCompOpAssignInverted, 80, 90, ctx);
+
+  // Some verbose for the watch
+  putstr("^= draw_image_effect", 0, 60, ctx);
+  putstr("draw_image_effect_pos =v", 0, 73, ctx);
+
+}
+
+static void	process(void *data)
+{
+  // We catch the user data
+  UserData	*userData = data;
+
+  // We create an Image (Before pushing any scenes to keep it until end of app !)
+  userData->img = create_img(RESOURCE_ID_DRAGON);
+
+  // We create scenes
+  create_basic_scene(graphic_callback1, NULL, NULL);
+  create_basic_scene(graphic_callback2, NULL, NULL);
+}
+
+int		main()
+{
+  // The user datas (it can be anything !)
+  UserData	data;
+
+  // We execute process function on startup
+  app_init(&data, process);
+}
