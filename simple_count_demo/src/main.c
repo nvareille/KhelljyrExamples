eec125b05f6366e35582c5059cd542a9c32dc7f6
Nicolas VAREILLE <nicolas.vareille@epitech.eu>
2015-03-11 10:28:14 +0100

    Add simple count demo

---
+++ simple_count_demo/src/main.c
@@ -0,0 +1,64 @@
+#include "Khelljyr/Khelljyr.h"
+
+static void	format(void *data, char *str, size_t size)
+{
+  int		*nbr = data;
+
+  snprintf(str, size, "%d", *nbr);
+}
+
+static void	graphic_add(Layer *l, GContext *ctx)
+{
+  putstr_format_font(10, format, USER_PTR, FONT_KEY_GOTHIC_28, 0, 0, ctx);
+}
+
+static void	load()
+{
+  int		*nbr = USER_PTR;
+  
+  *nbr = 0;
+}
+
+static void	add_10()
+{
+  int		*nbr = USER_PTR;
+
+  *nbr += 10;
+  refresh();
+}
+
+static void	add_1()
+{
+  int		*nbr = USER_PTR;
+
+  ++*nbr;
+  refresh();
+}
+
+static void	action()
+{
+  int		*nbr = USER_PTR;
+
+  --*nbr;
+  refresh();
+}
+
+static void	click()
+{
+  window_single_click_subscribe(BUTTON_ID_UP, add_10);
+  window_single_click_subscribe(BUTTON_ID_SELECT, action);
+  window_single_click_subscribe(BUTTON_ID_DOWN, add_1);
+}
+
+static void	exec()
+{
+  create_basic_scene(graphic_add, load, NULL, click);
+}
+
+int		main()
+{
+  int		nbr;
+
+  app_init(&nbr, exec);
+  return (0);
+}
